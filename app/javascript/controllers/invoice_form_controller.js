2138bd068e0df81712e3886eab02fca5660963c4
Patrick Morgan <patrick@patrick-morgan.net>
2022-10-20 19:08:44 -0700

    Fix Invoice edit form submit button

--- app/javascript/controllers/invoice_form_controller.js
+++ app/javascript/controllers/invoice_form_controller.js
@@ -1,7 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["file_field", "file_selected_image", "submit_button", "file_upload_prompt", "amount_field"]
+  static targets = ["file_field", "file_selected_image", "submit_button", "file_upload_prompt", "amount_field", "document"]
+
+  connect() {
+    const submit_button_el = this.submit_buttonTarget
+    if (this.file_present() && this.valid_amount()) {
+      submit_button_el.removeAttribute('disabled')
+    }
+  }
 
   file_changed(){
     const upload_image_el = this.file_upload_promptTarget
@@ -24,9 +31,21 @@ export default class extends Controller {
   }
 
   file_present() {
+    const hasdocument_el = this.documentTarget
+    console.log(hasdocument_el)
+    let has_document = null
+    if (hasdocument_el != null) {
+      has_document = hasdocument_el.dataset.hasdocument == 'true'
+    } else {
+      has_document = false
+    }
+    if (has_document) {
+      return(true)
+    }
+    console.log(has_document)
     const file_field_el = this.file_fieldTarget 
     const filename = file_field_el.value
-    return(filename.length > 10)
+    return(has_document || ( filename.length > 10 ))
   }
 
   valid_amount() {
