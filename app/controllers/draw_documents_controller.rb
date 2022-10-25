c815fe755dc4c0fa4cef643f5102e9449437a0a2
Patrick Morgan <patrick@patrick-morgan.net>
2022-10-25 14:31:56 -0700

    DrawDocuments WIP

--- app/controllers/draw_documents_controller.rb
+++ app/controllers/draw_documents_controller.rb
@@ -9,7 +9,6 @@ class DrawDocumentsController < ApplicationController
     authorize @draw_document
   end
 
-  # TODO
   def create
     @draw_document = DrawDocument.new(draw: @draw, user: @current_user)
     authorize @draw_document
@@ -17,10 +16,37 @@ class DrawDocumentsController < ApplicationController
     @draw_document = @service.create(params)
     respond_to do |format|
       if @service.errors?
-    
+        format.html { render :new, status: :unprocessable_entity  }
       else
+        format.html {
+          redirect_to project_draw_path(project_id: @draw.project_id, id: @draw.id), notice: 'Added Document' }
+        format.turbo_stream
       end
     end
+  end
+
+  def edit
+    authorize @draw_document
+  end
+
+  def update
+    authorize @draw_document
+    @service = DrawDocumentService.new(draw_document: @draw_document, user: @current_user)
+    @draw_document = @service.update(params)
+    respond_to do |format|
+      if @service.errors?
+        format.html { render :new, status: :unprocessable_entity }
+      else
+        format.html { redirect_to draw_draw_document_path(draw_id: @draw.id, id: @draw_document.id), notice: 'Updated Document' }
+        format.turbo_stream
+      end
+    end
+  end
+
+  def destroy
+    authorize @draw_document
+    @service = DrawDocumentService.new(draw_document: @draw_document, user: @current_user)
+    @service.remove
   end
 
 
