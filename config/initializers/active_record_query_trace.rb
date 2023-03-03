a11c4391c5cb1f9c9bd084fd8c85d0c548e21393
Patrick Morgan <patrick@patrick-morgan.net>
2023-03-03 15:20:42 -0800

    Allow toggle of ActiveRecordQueryTrace with the DEBUG envvar

--- config/initializers/active_record_query_trace.rb
+++ config/initializers/active_record_query_trace.rb
@@ -2,7 +2,7 @@
 #   https://github.com/brunofacca/active-record-query-trace
 
 if Rails.env.development?
-  ActiveRecordQueryTrace.enabled = true
+  ActiveRecordQueryTrace.enabled = ['1', 't', 'true'].include?(ENV.fetch('DEBUG', 'false'))
   # Optional: other gem config options go here:
   ActiveRecordQueryTrace.colorize = :light_purple
   #ActiveRecordQueryTrace.ignore_cached_queries = true
