df6d6855e33e019cef827a31752b344b751b9253
Patrick Morgan <patrick@patrick-morgan.net>
2023-03-27 23:04:56 -0700

    Unit test fixes

--- spec/policies/invoice_policy_spec.rb
+++ spec/policies/invoice_policy_spec.rb
@@ -12,53 +12,25 @@ RSpec.describe InvoicePolicy do
       draw.update(state: :submitted)
     end
 
-    describe 'when an invoice is clean' do
-      it 'should allow finance users to approve an invoice' do
-        user = sample_project.finance.first
-        refute(draw_cost.consult?)
-        refute(invoice1.consult?)
-        policy = InvoicePolicy.new(user, invoice1)
-        assert(policy.approve?)
-      end
+    it 'should allow finance users to approve an invoice' do
+      user = sample_project.finance.first
+      refute(draw_cost.consult?)
+      policy = InvoicePolicy.new(user, invoice1)
+      assert(policy.approve?)
     end
-    describe 'when an invoice is not clean' do
-      let(:change_order_amount) { 1000.0 }
-      let(:contingency_project_cost) {
-        cost = create(:project_cost, project: sample_project, name: 'Sample Contingency', total: 100000.0)
-        sample_project.project_costs.reload
-        cost
-      }
-      let(:change_order) {
-        project_cost = draw_cost.project_cost
-        contingency_project_cost
-        create(:change_order,
-               amount: change_order_amount,
-               draw_cost: draw_cost,
-               project_cost: draw_cost.project_cost,
-               funding_source: contingency_project_cost)
-      }
 
-      before do
-        change_order
-      end
+    it 'should allow manager users to approve an invoice' do
+      user = sample_project.managers.first
+      refute(draw_cost.consult?)
+      policy = InvoicePolicy.new(user, invoice1)
+      assert(policy.approve?)
+    end
 
-      it 'does not allow finance to approve the invoice' do
-        user = sample_project.finance.first
-        policy = InvoicePolicy.new(user, invoice1)
-        refute(policy.approve?)
-      end
-      it 'allows managers to approve the invoice' do
-        change_order
-        user = sample_project.managers.first
-        policy = InvoicePolicy.new(user, invoice1)
-        assert(policy.approve?)
-      end
-      it 'allows managers to approve the invoice' do
-        change_order
-        user = sample_project.owners.first
-        policy = InvoicePolicy.new(user, invoice1)
-        assert(policy.approve?)
-      end
+    it 'should not allow developer users to approve an invoice' do
+      user = sample_project.developers.first
+      refute(draw_cost.consult?)
+      policy = InvoicePolicy.new(user, invoice1)
+      refute(policy.approve?)
     end
   end
 end
